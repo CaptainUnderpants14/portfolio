6f4c32c4c1ab020f80187fc98cf4085240e4f86f
CaptainUnderpants14 <parassharma8890@gmail.com>
2026-05-31 16:51:34 +0530

    Final 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,  // Expose to the whole network
-    port: 5173,       // You can change this if needed
-  },
+    host: true,
+    port: 5173},
 })
